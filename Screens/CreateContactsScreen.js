5b4a749d156c4d7c3438eaacef38e97068866dd9
HK <hk540539@gmail.com>
2019-10-07 08:28:57 +0530

    Add Contact page update

--- Screens/CreateContactsScreen.js
+++ Screens/CreateContactsScreen.js
@@ -1,21 +1,59 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import {
+	Text,
+	View,
+	StyleSheet,
+	Keyboard,
+	AsyncStorage,
+	Alert,
+	TouchableWithoutFeedback,
+	ScrollView
+} from 'react-native';
+
+import { Form, Label, Button, Input, Item } from 'native-base';
 
 export default class CreateContactsScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text> CreateContactsScreen </Text>
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			fname: '',
+			lname: '',
+			phone: '',
+			email: '',
+			address: ''
+		};
+	}
+	static navigationOptions = {
+		title: 'Add Contact'
+	};
+
+	saveContact = () => {
+		if (
+			this.state.fname !== '' &&
+			this.state.lname !== '' &&
+			this.state.phone !== '' &&
+			this.state.email !== '' &&
+			this.state.address !== ''
+		) {
+		} else {
+			Alert.alert('All fields are required');
+		}
+	};
+
+	render() {
+		return (
+			<View>
+				<Text> CreateContactsScreen </Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
