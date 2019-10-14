ffb0b88a60928206bdd63c6deda2bd9d5654c974
hk540539 <hk540539@gmail.com>
2019-10-14 17:32:07 +0530

    grab key and populate data

--- Screens/ViewContactsScreen.js
+++ Screens/ViewContactsScreen.js
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  AsyncStorage,
+  ScrollView,
+  Linking,
+  Platform
+} from "react-native";
+
+import { Card, CardItem } from "native-base";
+import { Entypo } from "@expo/vector-icons";
 
 export default class ViewContactsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "dummy",
+      lname: "dummy",
+      phone: "dummy",
+      email: "dummy",
+      address: "dummy",
+      key: "dummy"
+    };
+  }
+  static navigationOptions = {
+    title: "View Contact"
+  };
+
+  componentDidMount() {
+    const { navigation } = this.props;
+    navigation.addListener("willFocus", () => {
+      var key = this.props.navigation.getParam("key", "");
+    });
+  }
+
   render() {
     return (
       <View>
