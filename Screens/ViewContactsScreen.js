8dff1aeb48abd1049f916c7ac0e8027366d9323c
HK <hk540539@gmail.com>
2019-10-15 10:02:12 +0530

    retrive data from async storage based on key as param, call action and sms action based on platform using linking

--- Screens/ViewContactsScreen.js
+++ Screens/ViewContactsScreen.js
@@ -1,54 +1,94 @@
-import React, { Component } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  AsyncStorage,
-  ScrollView,
-  Linking,
-  Platform
-} from "react-native";
-
-import { Card, CardItem } from "native-base";
-import { Entypo } from "@expo/vector-icons";
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, AsyncStorage, ScrollView, Linking, Platform } from 'react-native';
+import { Card, CardItem } from 'native-base';
+import { Entypo } from '@expo/vector-icons';
 
 export default class ViewContactsScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fname: "dummy",
-      lname: "dummy",
-      phone: "dummy",
-      email: "dummy",
-      address: "dummy",
-      key: "dummy"
-    };
-  }
-  static navigationOptions = {
-    title: "View Contact"
-  };
+	constructor(props) {
+		super(props);
+		this.state = {
+			fname: 'dummy',
+			lname: 'dummy',
+			phone: 'dummy',
+			email: 'dummy',
+			address: 'dummy',
+			key: 'dummy'
+		};
+	}
+	static navigationOptions = {
+		title: 'View Contact'
+	};
 
-  componentDidMount() {
-    const { navigation } = this.props;
-    navigation.addListener("willFocus", () => {
-      var key = this.props.navigation.getParam("key", "");
-    });
-  }
+	componentDidMount() {
+		const { navigation } = this.props;
+		navigation.addListener('willFocus', () => {
+			var key = this.props.navigation.getParam('key', '');
+			this.getContact(key);
+		});
+	}
 
-  render() {
-    return (
-      <View>
-        <Text> ViewContactsScreen </Text>
-      </View>
-    );
-  }
+	getContact = async (key) => {
+		await AsyncStorage.getItem(key)
+			.then((contactjsonString) => {
+				var contact = JSON.parse(contactjsonString);
+				contact['key'] = key;
+				this.setState(contact);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	callAction = (phone) => {
+		let phoneNumber = phone;
+		if (Platform.OS !== 'android') {
+			phoneNumber = `telpromt:${phone}`;
+		} else {
+			phoneNumber = `tel:${phone}`;
+		}
+		Linking.canOpenURL(phoneNumber)
+			.then((supported) => {
+				if (!supported) {
+					Alert.alert('Phone number is not available');
+				} else {
+					return Linking.openURL(phoneNumber);
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	smsAction = (phone) => {
+		let phoneNumber = phone;
+		phoneNumber = `sms:${phone}`;
+		Linking.canOpenURL(phoneNumber)
+			.then((supported) => {
+				if (!supported) {
+					Alert.alert('Phone number is not available');
+				} else {
+					return Linking.openURL(phoneNumber);
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	render() {
+		return (
+			<View>
+				<Text> ViewContactsScreen </Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
